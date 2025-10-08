81db908b89d1ea047387383125d039f933e0140d
doyeon <doyun20445@gmail.com>
2025-10-08 19:25:07 +0900

    remove: 불필요한 파일 삭제

--- src/main/java/NotFound/next_campus/domain/post/service/PostCategoryService.java
+++
@@ -1,8 +0,0 @@
-package NotFound.next_campus.domain.post.service;
-
-import java.util.List;
-
-public interface PostCategoryService {
-
-    void savePostCategories(Long postId, List<Long> categories);
-}
