dbf2a6ef7520688a5ef7760aa3963a2378910515
doyeon <doyun20445@gmail.com>
2025-10-08 22:16:41 +0900

    feat: 게시물 수정, 삭제 api 구현

--- src/main/java/NotFound/next_campus/domain/post/api/PostController.java
+++ src/main/java/NotFound/next_campus/domain/post/api/PostController.java
@@ -1,6 +1,7 @@
 package NotFound.next_campus.domain.post.api;
 
 import NotFound.next_campus.domain.post.dto.request.CreatePostDTO;
+import NotFound.next_campus.domain.post.dto.request.UpdatePostDTO;
 import NotFound.next_campus.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +35,41 @@ public class PostController {
 
         return ResponseEntity.ok().body(
                 "이미지 등록 성공"
+        );
+    }
+
+    @PatchMapping("/{post_id}")
+    public ResponseEntity<String> modifyPost(
+            @PathVariable("post_id") Long postId,
+            @RequestBody UpdatePostDTO request
+    ) {
+        postService.updatePost(postId, request);
+
+        return ResponseEntity.ok().body(
+                "게시물 수정 성공"
+        );
+    }
+
+    @PatchMapping("/{post_id}/images")
+    public ResponseEntity<String> modifyPostImage(
+            @PathVariable("post_id") Long postId,
+            @RequestParam("file") MultipartFile file
+    ) {
+        postService.updatePostImage(postId, file);
+
+        return ResponseEntity.ok().body(
+                "게시물 이미지 수정 성공"
+        );
+    }
+    
+    @DeleteMapping("/{post_id}")
+    public ResponseEntity<String> removePost(
+            @PathVariable("post_id") Long postId
+    ) {
+        postService.deletePost(postId, 1L);
+        
+        return ResponseEntity.ok().body(
+                "게시물 삭제 성공"
         );
     }
 }
