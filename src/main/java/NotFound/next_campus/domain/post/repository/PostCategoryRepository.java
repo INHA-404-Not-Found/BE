59619e1e26a485ca9b4351b5a7de4528bec2feae
doyeon <doyun20445@gmail.com>
2025-10-08 23:58:46 +0900

    feat: 게시물 카테고리 조회 함수 추가

--- src/main/java/NotFound/next_campus/domain/post/repository/PostCategoryRepository.java
+++ src/main/java/NotFound/next_campus/domain/post/repository/PostCategoryRepository.java
@@ -1,10 +1,15 @@
 package NotFound.next_campus.domain.post.repository;
 
+import NotFound.next_campus.domain.category.model.Category;
 import NotFound.next_campus.domain.post.model.Post;
 import NotFound.next_campus.domain.post.model.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
 
     void deleteByPost(Post post);
+
+    List<PostCategory> findByPost(Post post);
 }
