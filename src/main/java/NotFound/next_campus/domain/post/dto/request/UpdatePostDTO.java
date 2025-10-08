15bb13b06d90f875a8c4f03ca53009377746aeb8
doyeon <doyun20445@gmail.com>
2025-10-08 22:16:29 +0900

    feat: 게시물 수정 DTO 구현

---
+++ src/main/java/NotFound/next_campus/domain/post/dto/request/UpdatePostDTO.java
@@ -0,0 +1,27 @@
+package NotFound.next_campus.domain.post.dto.request;
+
+import NotFound.next_campus.domain.post.model.PostStatus;
+import NotFound.next_campus.domain.post.model.PostType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePostDTO {
+    private Long memberId;          // 게시자
+    private Long locationId;        // 발견 장소
+    private String locationDetail;  // 세부 발견 장소
+    private String title;           // 제목
+    private String content;         // 내용
+    private String storedLocation;  // 보관 위치
+    private PostStatus status;      // 완료 상태
+    private PostType type;          // 게시물 유형(분실/습득/공지)
+    private Boolean isPersonal;     // 개인정보 포함 여부
+    private List<Long> categories;  // 게시할 카테고리 목록
+}
