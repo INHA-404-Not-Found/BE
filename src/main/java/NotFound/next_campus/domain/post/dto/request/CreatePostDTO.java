549bb5aa1e8acbd263cbc37b8540954ba4407009
doyeon <doyun20445@gmail.com>
2025-10-08 22:17:00 +0900

    feat: 게시물 등록 DTO에 상태 필드 추가

--- src/main/java/NotFound/next_campus/domain/post/dto/request/CreatePostDTO.java
+++ src/main/java/NotFound/next_campus/domain/post/dto/request/CreatePostDTO.java
@@ -1,5 +1,6 @@
 package NotFound.next_campus.domain.post.dto.request;
 
+import NotFound.next_campus.domain.post.model.PostStatus;
 import NotFound.next_campus.domain.post.model.PostType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ public class CreatePostDTO {
     private String title;           // 제목
     private String content;         // 내용
     private String storedLocation;  // 보관 위치
+    private PostStatus status;
     private PostType type;          // 게시물 유형(분실/습득/공지)
     private Boolean isPersonal;     // 개인정보 포함 여부
     private List<Long> categories;  // 게시할 카테고리 목록
