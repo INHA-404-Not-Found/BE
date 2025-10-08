d1334bee971f2ca987e68e72ffee20ad23512619
doyeon <doyun20445@gmail.com>
2025-10-08 22:17:37 +0900

    feat: 게시물 카테고리 삭제 함수 구현

--- src/main/java/NotFound/next_campus/domain/post/repository/PostCategoryRepository.java
+++ src/main/java/NotFound/next_campus/domain/post/repository/PostCategoryRepository.java
@@ -1,7 +1,10 @@
 package NotFound.next_campus.domain.post.repository;
 
+import NotFound.next_campus.domain.post.model.Post;
 import NotFound.next_campus.domain.post.model.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
+
+    void deleteByPost(Post post);
 }
