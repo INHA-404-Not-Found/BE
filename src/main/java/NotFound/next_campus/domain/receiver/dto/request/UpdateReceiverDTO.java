f34055250bb73a7232c1261b2c6fe26e0a54ab43
doyeon <doyun20445@gmail.com>
2025-10-09 15:10:56 +0900

    feat: receiver DTO 구현

---
+++ src/main/java/NotFound/next_campus/domain/receiver/dto/request/UpdateReceiverDTO.java
@@ -0,0 +1,18 @@
+package NotFound.next_campus.domain.receiver.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateReceiverDTO {
+    private Long memberId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String studentId;
+}
