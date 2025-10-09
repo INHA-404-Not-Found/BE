bf20014132a5bacef9b1954796740c371ab2b4db
jiyun421 <ju4480@naver.com>
2025-10-09 22:44:59 +0900

    수정ífeat: category

---
+++ src/main/java/NotFound/next_campus/domain/category/dto/request/CategoryRequestDTO.java
@@ -0,0 +1,10 @@
+package NotFound.next_campus.domain.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequestDTO {
+    private String name; // 카테고리 이름
+}
