8d4d42fdc4e0a6a8528ccc5d9075671b04129e0b
doyeon <doyun20445@gmail.com>
2025-10-11 01:05:46 +0900

    feat: 일괄 수정/삭제, 키워드 검색 api 구현

--- src/main/java/NotFound/next_campus/domain/post/api/PostController.java
+++ src/main/java/NotFound/next_campus/domain/post/api/PostController.java
@@ -1,7 +1,9 @@
 package NotFound.next_campus.domain.post.api;
 
 import NotFound.next_campus.domain.post.dto.request.CreatePostDTO;
+import NotFound.next_campus.domain.post.dto.request.DeletePostDTO;
 import NotFound.next_campus.domain.post.dto.request.UpdatePostDTO;
+import NotFound.next_campus.domain.post.dto.request.UpdatePostStatusDTO;
 import NotFound.next_campus.domain.post.dto.response.PostResponseDTO;
 import NotFound.next_campus.domain.post.model.PostStatus;
 import NotFound.next_campus.domain.post.model.PostType;
@@ -74,6 +76,17 @@ public class PostController {
         );
     }
 
+    @PatchMapping("/update")
+    public ResponseEntity<String> modifyPosts(
+            @RequestBody UpdatePostStatusDTO request
+    ) {
+        postService.updateStatusOfPosts(request);
+
+        return ResponseEntity.ok().body(
+                "게시물 인계 여부 일괄 수정 성공"
+        );
+    }
+
     @DeleteMapping("/{post_id}")
     public ResponseEntity<String> removePost(
             @PathVariable("post_id") Long postId
@@ -82,6 +95,17 @@ public class PostController {
 
         return ResponseEntity.ok().body(
                 "게시물 삭제 성공"
+        );
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> removePosts(
+            @RequestBody DeletePostDTO request
+    ) {
+        postService.deletePosts(request.getPostIds(), request.getMemberId());
+
+        return ResponseEntity.ok().body(
+                "게시물 일괄 삭제 성공"
         );
     }
 
@@ -108,16 +132,25 @@ public class PostController {
             @RequestParam(value = "type", required = false) PostType type,
             @RequestParam(value = "location_id", required = false) Long locationId,
             @RequestParam(value = "category_id", required = false) Long categoryId
-            ) {
+    ) {
         return ResponseEntity.ok().body(
                 postService.getPostsByTags(status, type, locationId, categoryId)
         );
     }
 
-    /*@GetMapping("paging")
+    @GetMapping("/search")
+    public ResponseEntity<List<PostResponseDTO>> getPostsByKeyword(
+            @RequestParam("keyword") String keyword
+    ) {
+        return ResponseEntity.ok().body(
+                postService.getPostsByKeyword(keyword)
+        );
+    }
+
+    @GetMapping("paging")
     public ResponseEntity<Page<PostResponseDTO>> getPostsByPaging(Pageable pageable) {
         return ResponseEntity.ok().body(
                 postService.getPostsByPaging(pageable)
         );
-    }*/
+    }
 }
