67c7f8a5543a2103cd5fa98bef181ca00fae359b
doyeon <doyun20445@gmail.com>
2025-10-11 00:03:51 +0900

    feat: 게시물 필터링 JPA 함수 구현

--- src/main/java/NotFound/next_campus/domain/post/repository/PostRepository.java
+++ src/main/java/NotFound/next_campus/domain/post/repository/PostRepository.java
@@ -1,7 +1,26 @@
 package NotFound.next_campus.domain.post.repository;
 
 import NotFound.next_campus.domain.post.model.Post;
+import NotFound.next_campus.domain.post.model.PostStatus;
+import NotFound.next_campus.domain.post.model.PostType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT DISTINCT p FROM Post p " +
+            "WHERE (:status IS NULL or p.status = :status) " +
+            "AND (:type IS NULL or p.type = :type) " +
+            "AND (:locationId IS NULL or p.location.id = :locationId) " +
+            "AND (:categoryId IS NULL or EXISTS ( " +
+            "            SELECT 1 FROM PostCategory pc " +
+            "            WHERE pc.post = p AND pc.category.id = :categoryId" +
+            "            ))")
+    List<Post> findPostsByTags(@Param("status") PostStatus status,
+                               @Param("type") PostType type,
+                               @Param("locationId") Long locationId,
+                               @Param("categoryId") Long categoryId);
 }
