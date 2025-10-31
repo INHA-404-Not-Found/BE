a63c14663d989452deb0ca80411ee0d5e1f2e06a
doyeon <doyun20445@gmail.com>
2025-11-01 00:53:37 +0900

    feat: 이미지 파일 조회 경로 매핑 핸들러 추가

--- src/main/java/NotFound/next_campus/global/config/web/WebConfig.java
+++ src/main/java/NotFound/next_campus/global/config/web/WebConfig.java
@@ -2,15 +2,27 @@ package NotFound.next_campus.global.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private String filePath = System.getProperty("user.dir") + "/uploads/";
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://lost-inha.kro.kr")
                 .allowedMethods("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // /uploads/** -> 실제 폴더로 매핑
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + filePath);
     }
 }
