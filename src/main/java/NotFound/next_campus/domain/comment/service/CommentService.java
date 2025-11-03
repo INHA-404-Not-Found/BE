8fa3acf6cb255c0dd618b0c0f318837fd97490cd
doyeon <doyun20445@gmail.com>
2025-11-03 22:06:05 +0900

    refactor: Comment DTO 리팩토링

--- src/main/java/NotFound/next_campus/domain/comment/service/CommentService.java
+++ src/main/java/NotFound/next_campus/domain/comment/service/CommentService.java
@@ -1,8 +1,6 @@
 package NotFound.next_campus.domain.comment.service;
 
-import NotFound.next_campus.domain.comment.dto.request.CreateCommentDTO;
-import NotFound.next_campus.domain.comment.dto.request.UpdateCommentDTO;
-import NotFound.next_campus.domain.comment.dto.response.CommentResponseDTO;
+import NotFound.next_campus.domain.comment.dto.CommentDTO;
 import NotFound.next_campus.global.auth.user.CustomUserDetails;
 
 import java.util.List;
@@ -10,14 +8,14 @@ import java.util.List;
 public interface CommentService {
 
     /* 댓글 등록 */
-    Long saveComment(CreateCommentDTO dto, CustomUserDetails userDetails);
+    Long saveComment(CommentDTO.CreateRequest dto, CustomUserDetails userDetails);
 
     /* 댓글 수정 */
-    void updateComment(Long commentId, UpdateCommentDTO dto, CustomUserDetails userDetails);
+    void updateComment(Long commentId, CommentDTO.UpdateRequest dto, CustomUserDetails userDetails);
 
     /* 댓글 삭제 */
     void deleteComment(Long commentId, CustomUserDetails userDetails);
 
     /* 댓글 목록 조회 */
-    List<CommentResponseDTO> getCommentsByPost(Long postId);
+    List<CommentDTO.Response> getCommentsByPost(Long postId);
 }
