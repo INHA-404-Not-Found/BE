37d5becce168c903239ed27bf12cac2757e898c3
doyeon <doyun20445@gmail.com>
2025-11-04 23:54:34 +0900

    refactor: 알림 DTO 리팩토링

---
+++ src/main/java/NotFound/next_campus/domain/notification/dto/NotificationDTO.java
@@ -0,0 +1,42 @@
+package NotFound.next_campus.domain.notification.dto;
+
+import NotFound.next_campus.domain.notification.model.Notification;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class NotificationDTO {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateRequest {
+        private Long memberId;
+        private String title;
+        private String message;
+        private String link;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private String title;
+        private String message;
+        private String link;
+        private Boolean isRead;
+
+        public static Response toResponse(Notification notification) {
+
+            return Response.builder()
+                    .title(notification.getTitle())
+                    .message(notification.getMessage())
+                    .link(notification.getLink())
+                    .isRead(notification.getIsRead())
+                    .build();
+        }
+    }
+}
